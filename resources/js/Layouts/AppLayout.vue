f99a4e236721a6b4a18dee14b9222870d78a04dc
Mohammed Alhalwachi <Mohammed.abdameeralhalwachi@gmail.com>
2021-01-08 03:30:26 +0300

    show the user's profile photo on desktop navbar

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -34,8 +34,17 @@
                         <div class="relative ml-3">
                             <jet-dropdown align="right" width="48">
                                 <template #trigger>
-                                    <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm rounded-full border-2 border-transparent transition duration-150 ease-in-out focus:outline-none focus:border-gray-300">
-                                        <current-user-avatar />
+                                    <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex items-center text-sm rounded-full border-2 border-transparent transition duration-150 ease-in-out focus:outline-none">
+                                        <div class="flex items-center text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">
+                                            <div>
+                                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+
+                                            <div class="ml-1">{{ $page.props.user.name }}</div>
+                                        </div>
+                                        <current-user-avatar class="ml-2" />
                                     </button>
 
                                     <button v-else class="flex items-center text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">
