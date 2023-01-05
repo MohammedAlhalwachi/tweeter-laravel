88942f8794a6f0ff6f7eee9707c5b48071026168
Mohammed Alhalwachi <Mohammed.abdameeralhalwachi@gmail.com>
2023-01-05 19:14:45 +0300

    Migrate to render: build JS

--- scripts/00-laravel-delploy.sh
+++ scripts/00-laravel-delploy.sh
@@ -3,6 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+npm install
+npm run prod
+
 echo "Caching config..."
 php artisan config:cache
 
